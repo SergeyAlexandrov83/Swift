45631975f556119683fa5347480fc43b662969a9
Sergey Alexandrov <residx83@gmail.com>
2022-12-01 00:08:40 +0300

    Summary

--- AlexandrovIOS31_HW_1_1_AIOS.playground/Contents.swift
+++ AlexandrovIOS31_HW_1_1_AIOS.playground/Contents.swift
@@ -1,8 +1,33 @@
-protocol Flying {
+//protocol Flying {
+//    func fly(from: String, to: String)
+//}
+//
+//protocol Carrier {
+//    func load()
+//    func refuel()
+//}
+//
+//class Bird: Flying {
+//    func fly(from: String, to: String) {}
+//}
+//
+//class Airplane: Flying, Carrier {
+//
+//    func fly(from: String, to: String) {}
+//    func load() {}
+//    func refuel() {}
+//}
+
+//Задача 1
+//Абстракция
+//Инкапсуляция
+//Наследование
+//Полиморфизм
+protocol Instruments {
     func fly(from: String, to: String)
 }
 
-protocol Carrier {
+protocol Band {
     func load()
     func refuel()
 }
@@ -18,14 +43,11 @@ class Airplane: Flying, Carrier {
     func refuel() {}
 }
 
-//Задача 1
-//Абстракция
-//Инкапсуляция
-//Наследование
-//Полиморфизм
-//
+
+
 //Задача 2
 //Класс
+// Класс - Холодильник
 //Объект
 //Свойства объектов
 //Методы
